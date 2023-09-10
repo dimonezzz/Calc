a5d89bcec5b53b890abeaf27c50669cdc24b70d1
dimonezzz <dimknysh@yandex.ru>
2023-09-10 16:38:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите выражение:");
+        String string = in.nextLine();
+        calc(string);
+    }
+
+    public static String calc(String input) {
+        String[] string = input.split(" ");
+        if (string.length == 3) {
+            switch (string[1]) {
+                case "+":
+                    input = String.valueOf(Integer.parseInt(string[0]) + Integer.parseInt(string[2]));
+                    System.out.println(input);
+                    break;
+                case "-":
+                    input = String.valueOf(Integer.parseInt(string[0]) - Integer.parseInt(string[2]));
+                    System.out.println(input);
+                    break;
+                case "/":
+                    if (string[2].equals("0")) {
+                        System.out.println("Делить на 0 нельзя!");
+                    } else
+                    {input = String.valueOf(Integer.parseInt(string[0]) / Integer.parseInt(string[2]));
+                    System.out.println(input);}
+                    break;
+                case "*":
+                    input = String.valueOf(Integer.parseInt(string[0]) * Integer.parseInt(string[2]));
+                    System.out.println(input);
+                    break;
+                default:
+                    try {
+                        throw new Exception();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        } else try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
+}
